215357d12271f1f7c0611e46d1e4450fe1feef19
Joni Vrapi <jonivrapi@yahoo.com>
2022-11-13 12:24:09 -0500

    WIP

--- programming-assignments/programming-assignment3/median_of_three.py
+++ programming-assignments/programming-assignment3/median_of_three.py
@@ -1,3 +1,5 @@
+import math
+
 def QuickSort(A, p, r):
     if p < r:
         q = Partition(A, p, r)
@@ -5,9 +7,13 @@ def QuickSort(A, p, r):
         QuickSort(A, q + 1, r)
 
 def Partition(A, p, r):
-    x = A[r]
     i = p - 1
     j = p
+
+    if j - i >= 2:
+        x = MedianOfThree(A, p, r)
+    else:
+        x = A[r]
 
     for j in range(p, r):
         if A[j] <= x:
@@ -16,6 +22,17 @@ def Partition(A, p, r):
     A[i + 1], A[r] = A[r], A[i + 1]
 
     return i + 1
+
+def MedianOfThree(A, i, j):
+    k = math.floor((i + j) / 2)
+
+    tempArray = [A[i], A[k], A[j]]
+    tempArray.sort()
+    
+    medianIndex = 1
+    
+    return tempArray[medianIndex]
+    
 
 
 A = [1, 9, 2, 8, 5, 3, 7, 6, 4, 4, 4]
