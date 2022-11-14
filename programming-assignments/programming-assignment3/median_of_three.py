846455c16166d4161e808a1b61536fdf77eb28c6
Joni Vrapi <jonivrapi@yahoo.com>
2022-11-14 12:33:39 -0500

    WIP

--- programming-assignments/programming-assignment3/median_of_three.py
+++ programming-assignments/programming-assignment3/median_of_three.py
@@ -2,40 +2,40 @@ import math
 
 loopIterations = 0
 
-def QuickSort(A, lowIndex, highIndex):
-    if lowIndex < highIndex:
-        q = Partition(A, lowIndex, highIndex)
-        QuickSort(A, lowIndex, q - 1)
-        QuickSort(A, q + 1, highIndex)
+def QuickSort(A, p, r):
+    if p < r:
+        q = Partition(A, p, r)
+        QuickSort(A, p, q - 1)
+        QuickSort(A, q + 1, r)
 
-def Partition(A, lowIndex, highIndex):
-    i = lowIndex - 1
+def Partition(A, p, r):
+    i = p - 1
 
-    (pivotValue, pivotIndex) = MedianOfThree(A, lowIndex, highIndex)
-    A[highIndex], A[pivotIndex] = A[pivotIndex], A[highIndex]
+    (pivotValue, pivotIndex) = MedianOfThree(A, p, r)
+    A[r], A[pivotIndex] = A[pivotIndex], A[r]
 
-    for j in range(lowIndex, highIndex):
+    for j in range(p, r):
         global loopIterations 
         loopIterations += 1
         if A[j] <= pivotValue:
             i = i + 1
             A[i], A[j] = A[j], A[i]
-    A[i + 1], A[highIndex] = A[highIndex], A[i + 1]
+    A[i + 1], A[r] = A[r], A[i + 1]
 
     return i + 1
 
-def MedianOfThree(A, lowIndex, highIndex):
-    k = math.floor((lowIndex + highIndex) / 2)
+def MedianOfThree(A, p, r):
+    k = math.floor((p + r) / 2)
 
     #sorted runs in O(n) in this case
-    tempArray = sorted([(A[lowIndex], lowIndex), (A[k], k), (A[highIndex], highIndex)], key=lambda tup: tup[0])
+    tempArray = sorted([(A[p], p), (A[k], k), (A[r], r)], key=lambda tup: tup[0])
 
     medianIndex = 1
 
-    if highIndex - lowIndex >= 2:
+    if r - p >= 2:
         return tempArray[medianIndex]
     else:
-        return (A[highIndex], highIndex)
+        return (A[r], r)
     
     
     
