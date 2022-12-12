c1d77dc2ae21e26e08622238e5d3d7a2f13f06d6
Joni Vrapi <jonivrapi@yahoo.com>
2022-12-12 00:32:46 -0500

    refactored

--- programming-assignments/programming-assignment4/signal.py
+++ programming-assignments/programming-assignment4/signal.py
@@ -1,101 +1,90 @@
-# s = "100010101"
-# s = "101010101010101"
-# s = "100110011001"
-s = "001100110101011001100110010101111"
-
-
-x = "101"
-# y = '0'
-y = "010"
-
-xSet = []
-ySet = []
-noiseSet = []
-
-xMovingIndex = 0
-yMovingIndex = 0
-
-xCompleted = 0
-yCompleted = 0
-
-def moveYIndex():
-    global yMovingIndex
-    global yCompleted
-
-    if (yMovingIndex >= len(y) - 1):
-        yMovingIndex = 0
-        yCompleted += 1
-    else:
-        yMovingIndex += 1
-
-def moveXIndex():
-    global xMovingIndex
-    global xCompleted
+class Signal:
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
+        self.xSet = []
+        self.ySet = []
+        self.noiseSet = []
+        self.xMovingIndex = 0
+        self.yMovingIndex = 0
+        self.xCompleted = 0
+        self.yCompleted = 0
     
-    if (xMovingIndex >= len(x) - 1):
-        xMovingIndex = 0
-        xCompleted += 1
-    else:
-        xMovingIndex += 1
-
-def isIthCharacterOfX(character):
-    isIthCharacter = x[xMovingIndex] == character
-    return isIthCharacter
-
-def isIthCharacterOfY(character):
-    isIthCharacter = y[yMovingIndex] == character
-    return isIthCharacter
-
-def prune():
-    global xSet
-    global ySet
-    global xMovingIndex
-    global yMovingIndex
-    global noiseSet
-
-    # noiseSet = noiseSet + xSet + ySet
-
-    largestIndex = noiseSet[len(noiseSet) - 1]
-
-    xMovingIndex = yMovingIndex = 0
-    xSet[:] = [x for x in xSet if x < largestIndex]
-    ySet[:] = [y for y in ySet if y < largestIndex]
-
-
-for index in range(len(s)):
-    if (isIthCharacterOfX(s[index]) and isIthCharacterOfY(s[index])):
-        if (xCompleted > yCompleted):
-            ySet.append(index + 1)
-            moveYIndex()
-        elif (xCompleted == yCompleted):
-            if(xMovingIndex > yMovingIndex):
-                xSet.append(index + 1)
-                moveXIndex()
-            else:
-                ySet.append(index + 1)
-                moveYIndex()
+    def moveYIndex(self):
+        if (self.yMovingIndex >= len(self.y) - 1):
+            self.yMovingIndex = 0
+            self.yCompleted += 1
         else:
-            xSet.append(index + 1)
-            moveXIndex()
-        continue
+            self.yMovingIndex += 1
+
+    def moveXIndex(self):
+        if (self.xMovingIndex >= len(self.x) - 1):
+            self.xMovingIndex = 0
+            self.xCompleted += 1
+        else:
+            self.xMovingIndex += 1
+
+    def isIthCharacterOfX(self, character):
+        return self.x[self.xMovingIndex] == character
+
+    def isIthCharacterOfY(self, character):
+        return self.y[self.yMovingIndex] == character
+
+    def prune(self):
+        largestIndex = self.noiseSet[len(self.noiseSet) - 1]
+
+        self.xMovingIndex = self.yMovingIndex = 0
+        self.xSet[:] = [x for x in self.xSet if x < largestIndex]
+        self.ySet[:] = [y for y in self.ySet if y < largestIndex]
     
-    if (isIthCharacterOfX(s[index])):
-        xSet.append(index + 1)
-        moveXIndex()
-        continue
+    def process(self):
+        for index in range(len(self.s)):
+            if (self.isIthCharacterOfX(self.s[index]) and self.isIthCharacterOfY(self.s[index])):
+                if (self.xCompleted > self.yCompleted):
+                    self.ySet.append(index + 1)
+                    self.moveYIndex()
+                elif (self.xCompleted == self.yCompleted):
+                    if(self.xMovingIndex > self.yMovingIndex):
+                        self.xSet.append(index + 1)
+                        self.moveXIndex()
+                    else:
+                        self.ySet.append(index + 1)
+                        self.moveYIndex()
+                else:
+                    self.xSet.append(index + 1)
+                    self.moveXIndex()
+                continue
+            
+            if (self.isIthCharacterOfX(self.s[index])):
+                self.xSet.append(index + 1)
+                self.moveXIndex()
+                continue
 
-    if (isIthCharacterOfY(s[index])):
-        ySet.append(index + 1)
-        moveYIndex()
-        continue
+            if (self.isIthCharacterOfY(self.s[index])):
+                self.ySet.append(index + 1)
+                self.moveYIndex()
+                continue
 
-    if (not isIthCharacterOfX(s[index]) and not isIthCharacterOfY(s[index])):
-        noiseSet.append(index)
-        prune()
+            if (not self.isIthCharacterOfX(self.s[index]) and not self.isIthCharacterOfY(self.s[index])):
+                self.noiseSet.append(index)
+                self.prune()
 
-if (len(noiseSet)):
-    noiseSet = [x for x in range(1, len(s) + 1) if x not in xSet and x not in ySet]
+        if (len(self.noiseSet)):
+            self.noiseSet = [x for x in range(1, len(self.s) + 1) if x not in self.xSet and x not in self.ySet]
+        
+        print(f'xSet: {self.xSet}')
+        print(f'ySet: {self.ySet}')
+        print(f'noiseSet: {self.noiseSet}')
 
-print(f'xSet: {xSet}')
-print(f'ySet: {ySet}')
-print(f'noiseSet: {noiseSet}')
+s1 = Signal("101", "0", "100010101")
+s1.process()
+
+s2 = Signal("101", "010", "101010101010101")
+s2.process()
+
+s3 = Signal("101", "010", "001100110101011001100110010101111")
+s3.process()
+
+s4 = Signal("101", "010", "100110011001")
+s4.process()
